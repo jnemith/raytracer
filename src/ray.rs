b8ab20b9efc49fe090faea0ed6e1bfce774c3bef
jnemith <jn9159@gmail.com>
2020-05-27 13:17:32 -0400

    Move color function to World impl

--- src/ray.rs
+++ src/ray.rs
@@ -1,6 +1,4 @@
 use crate::vec3::Vector3;
-use crate::world::World;
-use rgb::RGB;
 
 #[derive(Debug)]
 pub struct Ray {
@@ -15,29 +13,5 @@ impl Ray {
 
     pub fn at(&self, t: f64) -> Vector3 {
         self.origin + (self.dir * t)
-    }
-
-    pub fn get_color(&self, world: &World, depth: u32) -> RGB<f64> {
-        if depth >= world.max_depth {
-            return RGB::new(0., 0., 0.);
-        }
-
-        if let Some(hr) = world.hit(&self) {
-            // let mr = hr.mat.scatter(&self, &hr);
-            if let Some(mr) = hr.mat.scatter(&self, &hr) {
-                let tmp = mr.scattered.get_color(world, depth + 1);
-                RGB::new(
-                    tmp.r * mr.attenuation.r,
-                    tmp.g * mr.attenuation.g,
-                    tmp.b * mr.attenuation.b,
-                )
-            } else {
-                RGB::new(0., 0., 0.)
-            }
-        } else {
-            let unit = self.dir.unit_vec();
-            let t = 0.5 * (unit.y + 1.);
-            RGB::new(1., 1., 1.) * (1. - t) + RGB::new(0.5, 0.75, 1.) * t
-        }
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -51,11 +51,11 @@ impl Camera {
 
 impl World {
     pub fn new() -> World {
-        let spp = 15;
+        let spp = 10;
         let max_depth = 10;
 
         let aspect_ratio = 16. / 9.;
-        let width = 768;
+        let width = 500;
         let height = (width as f64 / aspect_ratio) as u32;
 
         World {
@@ -79,12 +79,40 @@ impl World {
                     let u = (col as f64 + rng.gen_range(0., 1.)) / (self.width as f64);
                     let v = (row as f64 + rng.gen_range(0., 1.)) / (self.height as f64);
                     let r = self.cam.ray(u, v);
-                    color += r.get_color(&self, 0);
+                    color = color + self.get_color(&r, 0);
                 }
                 self.write_color(color, col, index as u32);
             }
         }
         self.output.save(filename).unwrap();
+    }
+
+    pub fn get_color(&self, ray: &Ray, depth: u32) -> RGB<f64> {
+        if depth >= self.max_depth {
+            return RGB::new(0., 0., 0.);
+        }
+
+        if let Some(hr) = self.hit(&ray) {
+            if let Some(mr) = hr.mat.scatter(&ray, &hr) {
+                let att = RGB::new(
+                    mr.attenuation.r,
+                    mr.attenuation.g,
+                    mr.attenuation.b,
+                );
+                let tmp = self.get_color(&mr.scattered, depth + 1);
+                RGB::new(
+                    tmp.r * att.r,
+                    tmp.g * att.g,
+                    tmp.b * att.b,
+                )
+            } else {
+                RGB::new(0., 0., 0.)
+            }
+        } else {
+            let unit = ray.dir.unit_vec();
+            let t = 0.5 * (unit.y + 1.);
+            RGB::new(1., 1., 1.) * (1. - t) + RGB::new(0.5, 0.75, 1.) * t
+        }
     }
 
     pub fn hit(&self, ray: &Ray) -> Option<HitResult> {
